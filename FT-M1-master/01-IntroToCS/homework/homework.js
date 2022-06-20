cefb9d0154a063048e4a097370d4168a972b84d1
soletiagu <agustinsoleti95@gmail.com>
2022-06-20 12:09:16 -0300

    Terminado

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,30 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+var suma =0;
+num = num.toString().split("").reverse().join("")
+for (let item in num) {
+  if (num[item] !=0 ) {
+suma +=Math.pow(2,item);
+}
+}
+return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+let result = [];
+do {
+  if (num%2==0){
+    num /=2;
+    result.unshift(0);
+  }else{
+    result.unshift(1);
+    num=Math.floor(num/2);
+  }
+  
+} while (num>=1);
+return result.join("")
 }
 
 

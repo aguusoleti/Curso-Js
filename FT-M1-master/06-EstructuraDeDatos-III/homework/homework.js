a56c3e7f27a1cf8d99034bb6fb7fa7981134d176
aguusoleti <agustin_soleti@hotmail.com>
2022-07-13 23:50:00 -0300

    Casi terminado

--- FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
@@ -16,36 +16,62 @@ function BinarySearchTree(value) {
   this.rigth=null;
   this.left=null;
 }
-
-// function Node (){
-//   this.value=null;
-//   this.rigth=null;
-//   this.left=null;
-// }
-BinarySearchTree.prototype.size = function (){}
+BinarySearchTree.prototype.size = function (){
+   if(this.rigth === null && this.left === null) return 1
+   if (this.rigth !== null && this.left === null)return 1 + this.rigth.size()
+   if (this.rigth === null && this.left !== null)return 1 + this.left.size()
+   if (this.rigth !== null && this.left !== null)return 1 + this.left.size() + this.rigth.size()
+}
 BinarySearchTree.prototype.insert = function (value){
   if(value>this.value){
-    if(this.rigth=== null){ this.rigth=new BinarySearchTree(value)}
-    else {this.rigth === this.rigth.insert(value)}
-  }else{
-    if(this.left=== null){ this.left=new BinarySearchTree(value)}
-    else (this.left === this.left.insert(value))
+    if(this.rigth === null){ 
+      this.rigth = new BinarySearchTree(value)
+    }else {
+      this.rigth.insert(value)}
   }
-
+if (value < this.value) {
+    if(this.left=== null){ 
+      this.left=new BinarySearchTree(value)
+    }
+    else{
+      this.left.insert(value)}
+    }
 }
-BinarySearchTree.prototype.contains = function ( value){
-  if (this.value === value) {return true}
+BinarySearchTree.prototype.contains = function (value){
+  
+  if (this.value === value) return true
   if(value  > this.value){
-    if(this.rigth === null){ return false;}
-    else return this.rigth.contains(value)}
-  else{
+    if(this.rigth === null) return false;
+    else return this.rigth.contains(value)
+  }
+  
     if(value < this.value){
       
-      if(this.left === null){ return false;}
-      else return this.left.contains(value)}
-  }
+      if(this.left === null) return false;
+      else return this.left.contains(value)
+    }
+  
 }
-BinarySearchTree.prototype.depthFirstForEach = function (){}
+BinarySearchTree.prototype.depthFirstForEach = function (cb,orden){
+if (orden === 'pre-order'){
+// root - izq - der
+cb(this.value)
+if(this.left !== null) this.left.depthFirstForEach(cb,orden)
+if(this.rigth !== null) this.rigth.depthFirstForEach(cb,orden)
+}else if (orden === 'post-order'){
+  //izq - der - root
+  if(this.left !== null)this.left.depthFirstForEach(cb,orden)
+  if(this.rigth !== null) this.rigth.depthFirstForEach(cb,orden)
+  cb(this.value)
+  
+}else {
+// in-order   izq - root - der 
+if(this.left !== null ) this.left.depthFirstForEach(cb,orden)
+cb(this.value)
+if(this.rigth !== null) this.rigth.depthFirstForEach(cb,orden)
+}
+
+}
 BinarySearchTree.prototype.breadthFirstForEach = function (){}
 
 // No modifiquen nada debajo de esta linea

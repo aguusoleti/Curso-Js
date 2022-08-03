687a7c132d066715017c7f681ef827186df4cc48
soletiagu <agustinsoleti95@gmail.com>
2022-08-03 17:00:10 -0300

    casi terminado

--- FT-M2-master/FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/FT-M2-master/03-ES6/homework/src/selector.js
@@ -18,7 +18,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0]=== '#'){
+    return 'id'
+  }else if(selector[0]==='.'){
+    return 'class'
+  }else if(selector.split('.').length>1){
+    return 'tag.class'
+  }else{
+    return 'tag'
+  }
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +38,28 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = function(el){
+      return '#'+ el.id === selector
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(el){
+      let classes = el.classList
+      // classes.forEach(e => { if (`.${e}` === selector)return true;})
+      // return false
+    for (let i=0; i<classes.length;i++){
+      if (`.${classes[i]}` === selector)return true
+    }
+    return false
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function (el){
+      var [tagBuscado,classBuscada]= selector.split('.')
+      return matchFunctionMaker (tagBuscado)(el) && matchFunctionMaker(`.${classBuscada}`)(el)
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction =function (el){
+return el.tagName.toLowerCase() === selector
+    }
   }
   return matchFunction;
 };

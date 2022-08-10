1f12a8d5c60d38f0564ba4d7faeca382b8e55f96
aguusolelti <agustin_soleti@hotmail.com>
2022-08-10 20:12:44 -0300

    terminado

---
+++ FT-M2-master/FT-M2-master/05-Bundlers/homework/browser/bundle.js
@@ -0,0 +1,1 @@
+(()=>{var o,n;o=window.whiteboard,(n=window.io(window.location.origin)).on("connect",(function(){console.log("Connected!")})),n.on("load",(function(n){n.forEach((function(n){var i=n.start,a=n.end,c=n.color;o.draw(i,a,c,!1)}))})),n.on("draw",(function(n,i,a){o.draw(n,i,a,!1)})),o.on("draw",(function(o,i,a){n.emit("draw",o,i,a)}))})();
